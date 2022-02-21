ad2a89aacb5042b7b7e7aef6beb511c7adfdd9f9
koladev <onaelmangabo@gmail.com>
2022-02-22 00:19:12 +0100

    - test: add create User thunk

--- src/store/slices/__tests__/users.test.ts
+++ src/store/slices/__tests__/users.test.ts
@@ -1,110 +1,147 @@
-import reducer, {
-    initialState,
-    findUserById,
-    fetchUsers
-  } from "../user";
-  import axios from "axios";
-  import MockAdapter from "axios-mock-adapter";
-  import { store } from "../..";
-  
-  const userId = 1;
-  
-  const getUserResponse = {
-        "id": 1,
-        "name": "Leanne Graham",
-        "username": "Bret",
-        "email": "Sincere@april.biz",
-        "city": "Gwenborough",
-        "phone": "1-770-736-8031 x56442",
-        "website": "hildegard.org"
-  };
+import reducer, { initialState, findUserById, fetchUsers, addUser } from "../user";
+import axios from "axios";
+import MockAdapter from "axios-mock-adapter";
+import { store } from "../..";
 
-  const getUserListResponse = [
-    {
-        "id": 1,
-        "name": "Leanne Graham",
-        "username": "Bret",
-        "email": "Sincere@april.biz",
-        "city": "Gwenborough",
-        "phone": "1-770-736-8031 x56442",
-        "website": "hildegard.org"
-    },
-    {
-        "id": 2,
-        "name": "Ervin Howell",
-        "username": "Antonette",
-        "email": "ervin@april.biz",
-        "city": "Gwenborough",
-        "phone": "1-770-736-8031",
-        "website": "elvis.com"
+const userId = 1;
+
+const getUserResponse = {
+  id: 1,
+  name: "Leanne Graham",
+  username: "Bret",
+  email: "Sincere@april.biz",
+  city: "Gwenborough",
+  phone: "1-770-736-8031 x56442",
+  website: "hildegard.org",
+};
+
+const getCreateUserResponse = {
+    id: 3,
+    "name": "Clementine Bauch",
+    "username": "Samantha",
+    "email": "Nathan@yesenia.net",
+    "city": "Gwenborough",
+    "phone": "1-463-123-4447",
+    "website": "ramiro.info"
   }
-  ]
+
+const getUserListResponse = [
+  {
+    id: 1,
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "Sincere@april.biz",
+    city: "Gwenborough",
+    phone: "1-770-736-8031 x56442",
+    website: "hildegard.org",
+  },
+  {
+    id: 2,
+    name: "Ervin Howell",
+    username: "Antonette",
+    email: "ervin@april.biz",
+    city: "Gwenborough",
+    phone: "1-770-736-8031",
+    website: "elvis.com",
+  },
+];
+
+const mockNetWorkResponse = () => {
+  const mock = new MockAdapter(axios);
+  mock.onGet(`/users/?id=${userId}`).reply(200, getUserResponse);
+  mock.onGet(`/users/`).reply(200, getUserListResponse);
+    mock.onPost(`/users/`).reply(200, getCreateUserResponse);
+};
+
+/**
+ * Testing the initial state
+ */
+
+test("Should return initial state", () => {
+  expect(
+    reducer(undefined, {
+      type: undefined,
+    })
+  ).toEqual(initialState);
+});
+
+/**
+ * Testing the findUserById thunk
+ */
+
+describe("User List state tests", () => {
+  beforeAll(() => {
+    mockNetWorkResponse();
+  });
+
+  it("Shoudl be able to fetch the user object", async () => {
+    const result = await store.dispatch(findUserById(userId));
+
+    const user = result.payload;
+
+    expect(result.type).toBe("users/findUserById/fulfilled");
+    expect(user).toEqual(getUserResponse);
+
+    const state = store.getState().users;
+
+    expect(state.users).toEqual([getUserResponse]);
+  });
+});
+
+/**
+ * Testing the findUserById thunk
+ */
+
+describe("List all users", () => {
+  beforeAll(() => {
+    mockNetWorkResponse();
+  });
+
+  it("Shoudl be able to fetch the user list", async () => {
+    const result = await store.dispatch(fetchUsers());
+
+    const users = result.payload;
+
+    expect(result.type).toBe("users/fetchUsers/fulfilled");
+    expect(users).toEqual(getUserListResponse);
+
+    const state = store.getState().users;
+
+    expect(state.users).toEqual(getUserListResponse);
+  });
+});
+
+
+/**
+ * Testing the createUser thunk
+ */
+
+ describe("Create a new user", () => {
+    beforeAll(() => {
+      mockNetWorkResponse();
+    });
   
+    it("Should be able to fetch the user list", async () => {
+      // Saving previous state
+      const previousState = store.getState().users;
+
+      const previousUsers = [
+          ...previousState.users
+      ];
+      previousUsers.push(getCreateUserResponse);
+
+      // Dispatching the action
+
+      const result = await store.dispatch(addUser(getCreateUserResponse));
   
-  const mockNetWorkResponse = () => {
-    const mock = new MockAdapter(axios);
-    mock.onGet(`/users/?id=${userId}`).reply(200, getUserResponse);
-    mock.onGet(`/users/`).reply(200, getUserListResponse);
-  }
+      const user = result.payload;
+
+      expect(result.type).toBe("users/addUser/fulfilled");
+      expect(user).toEqual(getCreateUserResponse);
   
+      const state = store.getState().users;
   
-  /**
-   * Testing the initial state
-   */
-  
-  test("Should return initial state", () => {
-    expect(
-      reducer(undefined, {
-        type: undefined,
-      })
-    ).toEqual(initialState);
+      expect(state.users).toEqual(previousUsers);
+    });
   });
   
-  /**
-   * Testing the findUserById thunk
-   */
-  
-  describe("User List state tests", () => {
-      beforeAll(() => {
-        mockNetWorkResponse();
-      })
-  
-      it("Shoudl be able to fetch the user object", async () => {
-        const result = await store.dispatch(findUserById(userId));
-  
-        const user = result.payload;
-  
-        expect(result.type).toBe("users/findUserById/fulfilled");
-        expect(user).toEqual(getUserResponse);
-  
-        const state = store.getState().users;
-  
-        expect(state.users).toEqual([getUserResponse]);
-      })
-      
-  }); 
-
-
-    /**
-   * Testing the findUserById thunk
-   */
-  
-     describe("List all users", () => {
-        beforeAll(() => {
-          mockNetWorkResponse();
-        })
-    
-        it("Shoudl be able to fetch the user list", async () => {
-          const result = await store.dispatch(fetchUsers());
-    
-          const users = result.payload;
-    
-          expect(result.type).toBe("users/fetchUsers/fulfilled");
-          expect(users).toEqual(getUserListResponse);
-    
-          const state = store.getState().users;
-    
-          expect(state.users).toEqual(getUserListResponse);
-        })
-        
-    }); 
