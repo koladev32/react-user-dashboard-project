3d8fb34e878aa9f8a9f39a5d69d302ebafc6a25b
koladev <onaelmangabo@gmail.com>
2022-02-21 23:45:10 +0100

    - feat: register user

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import {combineReducers} from "redux";
+import {userSlice} from "./slices/user";
 
 const rootReducer = combineReducers({
-  
+  users: userSlice.reducer,
 });
 
 export const store = configureStore({
