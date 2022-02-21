f51fab39e0b5e3c1904c7a3af182192da1a76b5d
koladev <onaelmangabo@gmail.com>
2022-02-22 00:59:23 +0100

    - ui: add delete user component

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,6 @@ import Layout from "./Layout";
 import UserTable from "./UserTable";
 import AddUser from "./forms/AddUser";
 import UpdateUser from "./forms/UpdateUser";
+import DeleteUser from "./forms/DeleteUser";
 
-export { Layout, AddUser, UpdateUser, UserTable };
+export { Layout, AddUser, UpdateUser, UserTable, DeleteUser };
