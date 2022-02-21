df6e0aaa3a06b9a64589f6d829e8024a069eadff
koladev <onaelmangabo@gmail.com>
2022-02-22 00:58:17 +0100

    - ui: add forms component

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+import Layout from "./Layout";
+import UserTable from "./UserTable";
+import AddUser from "./forms/AddUser";
+import UpdateUser from "./forms/UpdateUser";
+
+export { Layout, AddUser, UpdateUser, UserTable };
